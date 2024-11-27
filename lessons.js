8130506a8d105a5bda5b27d4f164a633c8560d68
Panashe Tapfumaneyi <99790286+PanasheTapfumaneyi@users.noreply.github.com>
2024-11-27 19:43:32 +0400

    Added server

---
+++ lessons.js
@@ -0,0 +1,21 @@
+
+
+let lessons = [
+    { id: 1, 
+        title: "Math 101", 
+        description: "Basic Math Lessons", 
+        price: 50, 
+        availableInventory: 5, 
+        rating: 4, 
+        image: "/assets/math.png" 
+    },
+    { id: 2, 
+        title: "History Basics", 
+        description: "World History Overview", 
+        price: 75, 
+        availableInventory: 3, 
+        rating: 5, 
+        image: "/assets/history.png" 
+    },
+    
+]
